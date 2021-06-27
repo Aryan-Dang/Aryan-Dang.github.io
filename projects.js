062290c114bbacd7b05e8e213c361c09f10b072a
Aryan <aryandang43@gmail.com>
2021-06-27 14:38:33 +0530

    Updated `index.html` and `projects.js`
    Made loading project count from javascript file smoother.
    
    1) added `onload` event to <body> so that project count is set when body has been loaded.
    2) removed <script>
    3) Added function which modifies DOM to accurately display project count.

--- projects.js
+++ projects.js
@@ -5,3 +5,6 @@ let projects = [
         'description' : 'A sample project'
     }
 ];
+
+//sets the projects count according to the length of projects
+const setF = () => { document.getElementById('projectsBadge').innerHTML=projects.length; }
