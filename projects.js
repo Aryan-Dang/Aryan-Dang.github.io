82f8df7919a0e1e1a7757cc8d029040e0f856de3
Aryan <aryandang43@gmail.com>
2021-08-21 02:23:25 +0530

    adds Bootstrap code for diplaying project card

--- projects.js
+++ projects.js
@@ -31,9 +31,15 @@ function displayProject(project){
     let wrapper = document.getElementById('project-wrapper');
     //using += adds new project
     wrapper.innerHTML += `
-    <div class="project">
-        <h2 class='project-name'>${project.name}</h2>
-        <p class='project-desc'>${project.description}</p>
+        <!--bootstrap code for displaying card-->
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+        </div>
     </div>
     `;
 }
